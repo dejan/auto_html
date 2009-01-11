415ec359c4b0e97375794ba705cdb418e4a7c73e
Dejan Simic <desimic@gmail.com>
2009-01-11 21:29:43 +0100

    Changed regexp for matching images. Old one didn't match URL with subdomains

--- lib/auto_html/filters/image.rb
+++ lib/auto_html/filters/image.rb
@@ -5,10 +5,9 @@ module AutoHtml
       image = auto_html_match_image(url)
       image && %|<img src="#{image}" alt=""/>|
     end
-
-    # http://stackoverflow.com/questions/169625/regex-to-check-if-valid-url-that-ends-in-jpg-png-or-gif
+    
     def auto_html_match_image(url)
-      url[/https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpg|gif|png)/]
+      url[/http:\/\/.+(jpg|gif|png)/]
     end
   end
 end
