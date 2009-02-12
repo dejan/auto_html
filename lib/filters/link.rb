862432a73e301773b357646d6c3858d122741bd1
Dejan Simic <desimic@gmail.com>
2009-02-12 14:21:07 +0100

    Copied/adjusted some code from ActionView::Helpers::TagHelper & TextHelper to escape the need to require/include those modules

--- lib/filters/link.rb
+++ lib/filters/link.rb
@@ -1,17 +1,48 @@
+# major portion of code in here is copied/adjusted from 
+# ActionView::Helpers::TagHelper and
+# ActionView::Helpers::TextHelper
+#
 AutoHtml.add_filter(:link).with(:html_options => {}) do |text, options|
-  require 'action_view'
-  include ActionView::Helpers::TagHelper
-  include ActionView::Helpers::TextHelper
+  
+  def tag_options(options)
+    unless options.blank?
+      attrs = []
+      attrs = options.map { |key, value| %(#{key}="#{value}") }
+      " #{attrs.sort * ' '}" unless attrs.empty?
+    end
+  end
+
+  def auto_link_re
+    %r{
+      (                          # leading text
+        <\w+.*?>|                # leading HTML tag, or
+        [^=!:'"/]|               # leading punctuation, or
+        ^                        # beginning of line
+      )
+      (
+        (?:https?://)|           # protocol spec, or
+        (?:www\.)                # www.*
+      )
+      (
+        [-\w]+                   # subdomain or domain
+        (?:\.[-\w]+)*            # remaining subdomains or domain
+        (?::\d+)?                # port
+        (?:/(?:[~\w\+@%=\(\)-]|(?:[,.;:'][^\s$]))*)* # path
+        (?:\?[\w\+@%&=.;:-]+)?     # query string
+        (?:\#[\w\-]*)?           # trailing anchor
+      )
+      ([[:punct:]]|<|$|)       # trailing text
+     }x
+  end
 
   html_options = options[:html_options]
   extra_options = tag_options(html_options.stringify_keys) || ""
-  text.gsub(AUTO_LINK_RE) do
+  text.gsub(auto_link_re) do
     all, a, b, c, d = $&, $1, $2, $3, $4
     if a =~ /<a\s/i # don't replace URL's that are already linked
       all
     else
       text = b + c
-      text = yield(text) if block_given?
       %(#{a}<a href="#{b=="www."?"http://www.":b}#{c}"#{extra_options}>#{text}</a>#{d})
     end
   end
