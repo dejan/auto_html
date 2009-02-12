4eb329f3479c35daa7fd46ebec34c79570d948e9
Dejan Simic <desimic@gmail.com>
2009-02-12 17:52:47 +0100

    Removed duplicated code.

--- init.rb
+++ init.rb
@@ -1,9 +1,3 @@
 require 'auto_html'
 
 ActiveRecord::Base.send :include, AutoHtmlFor
-
-# Register built-in filters
-#
-Dir["#{File.dirname(__FILE__) + '/auto_html/filters'}/**/*"].each do |filter|
-  require "#{filter}"
-end
