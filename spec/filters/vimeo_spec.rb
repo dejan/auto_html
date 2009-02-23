43638e6ccfd3a25f0f940a5bf48f470f7d214d4b
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-02-23 23:18:57 +0100

    Filter vimeo URLs with or without www.

--- spec/filters/vimeo_spec.rb
+++ spec/filters/vimeo_spec.rb
@@ -5,6 +5,14 @@ describe AutoHtml do
 
     auto_html("http://www.vimeo.com/3300155") { vimeo }.should == 
       '<object width="440" height="248"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=3300155&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00adef&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=3300155&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00adef&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="440" height="248"></embed></object>'
+    
+  end
+  
+  it 'should transform URL (without www.) to Vimeo embed markup' do
+
+    auto_html("http://vimeo.com/3300155") { vimeo }.should == 
+      '<object width="440" height="248"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=3300155&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00adef&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=3300155&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00adef&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="440" height="248"></embed></object>'
+  
   end
   
   it 'should transform URL to Vimeo embed markup with tweaked width & height' do
