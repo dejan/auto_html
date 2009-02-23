cb82c6051cbf12ee44f9e2cf70f5b414075430ee
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-02-23 20:41:52 +0100

    Add vimeo filter

---
+++ spec/filters/vimeo_spec.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe AutoHtml do
+  it 'should transform URL to Vimeo embed markup' do
+
+    auto_html("http://www.vimeo.com/3300155") { vimeo }.should == 
+      '<object width="440" height="248"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=3300155&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00adef&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=3300155&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00adef&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="440" height="248"></embed></object>'
+  end
+  
+  it 'should transform URL to Vimeo embed markup with tweaked width & height' do
+
+    auto_html("http://www.vimeo.com/3300155") { vimeo(:width => 300, :height => 250) }.should == 
+      '<object width="300" height="250"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=3300155&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00adef&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=3300155&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00adef&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="300" height="250"></embed></object>'
+
+  end
+end
