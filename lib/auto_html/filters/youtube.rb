e7a0f18e4a2869d31a345c200b4b2b5ca8bfa36f
Dejan Simic <desimic@gmail.com>
2009-05-20 21:40:58 +0200

    Fixed youtube filter to correctly handle param in URL

--- lib/auto_html/filters/youtube.rb
+++ lib/auto_html/filters/youtube.rb
@@ -1,5 +1,5 @@
 AutoHtml.add_filter(:youtube).with(:width => 390, :height => 250) do |text, options|
-  text.gsub(/http:\/\/(www.)?youtube\.com\/watch\?v=([A-Za-z0-9._%-]*)[&\w;=\+_\-]*/) do
+  text.gsub(/http:\/\/(www.)?youtube\.com\/watch\?v=([A-Za-z0-9._%-]*)(\&\S+)?/) do
     youtube_id = $2
     %{<object width="#{options[:width]}" height="#{options[:height]}"><param name="movie" value="http://www.youtube.com/v/#{youtube_id}"></param><param name="wmode" value="transparent"></param><embed src="http://www.youtube.com/v/#{youtube_id}" type="application/x-shockwave-flash" wmode="transparent" width="#{options[:width]}" height="#{options[:height]}"></embed></object>}
   end
--- spec/filters/youtube_spec.rb
+++ spec/filters/youtube_spec.rb
@@ -6,6 +6,12 @@ describe AutoHtml do
     auto_html("http://www.youtube.com/watch?v=BwNrmYRiX_o") { youtube }.should == 
       '<object width="390" height="250"><param name="movie" value="http://www.youtube.com/v/BwNrmYRiX_o"></param><param name="wmode" value="transparent"></param><embed src="http://www.youtube.com/v/BwNrmYRiX_o" type="application/x-shockwave-flash" wmode="transparent" width="390" height="250"></embed></object>'
 
+    auto_html("http://www.youtube.com/watch?v=3-ewi9saKg8&eurl=http%3A%2F%2Fvukajlija.com%2Fvideo%2Fklipovi%3Fstrana%3D6&feature=player_embedded") { youtube }.should == 
+      '<object width="390" height="250"><param name="movie" value="http://www.youtube.com/v/3-ewi9saKg8"></param><param name="wmode" value="transparent"></param><embed src="http://www.youtube.com/v/3-ewi9saKg8" type="application/x-shockwave-flash" wmode="transparent" width="390" height="250"></embed></object>'
+
+    auto_html("http://www.youtube.com/watch?v=Mw8KJ29qph0&feature=related") { youtube }.should == 
+      '<object width="390" height="250"><param name="movie" value="http://www.youtube.com/v/Mw8KJ29qph0"></param><param name="wmode" value="transparent"></param><embed src="http://www.youtube.com/v/Mw8KJ29qph0" type="application/x-shockwave-flash" wmode="transparent" width="390" height="250"></embed></object>'
+
   end
   
   it 'should transform URL (without www.) to YouTube embed markup' do
