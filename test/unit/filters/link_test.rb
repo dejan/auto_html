39ae8923bb13c2cb70ecc30c953bf63130ce2c08
Claudio Perez Gamayo <crossblaim@gmail.com>
2009-07-10 20:53:57 +0200

    Fix link filter for urls like: http://www.google.com/#q=nikola+tesla&ct=tesla09&oi=ddle&fp=Xmf0jJ9P_V0

--- test/unit/filters/link_test.rb
+++ test/unit/filters/link_test.rb
@@ -6,7 +6,12 @@ class LinkTest < Test::Unit::TestCase
     result = auto_html("http://vukajlija.com") { link }
     assert_equal '<a href="http://vukajlija.com">http://vukajlija.com</a>', result
   end
-
+  
+  def test_transform_complex_url
+    result = auto_html("http://www.google.com/#q=nikola+tesla&ct=tesla09&oi=ddle&fp=Xmf0jJ9P_V0") { link }
+    assert_equal '<a href="http://www.google.com/#q=nikola+tesla&ct=tesla09&oi=ddle&fp=Xmf0jJ9P_V0">http://www.google.com/#q=nikola+tesla&ct=tesla09&oi=ddle&fp=Xmf0jJ9P_V0</a>', result
+  end
+  
   def test_transform_with_options
     result = auto_html("http://rors.org") { link :rel => "nofollow", :target => "_blank" }
     assert_equal '<a href="http://rors.org" rel="nofollow" target="_blank">http://rors.org</a>', result
