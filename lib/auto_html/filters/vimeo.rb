61242ea732f2a31bad63016612d89e09556d6d55
Matt Polito <matt.polito@gmail.com>
2010-08-21 06:39:03 +0800

    added vimeo html5 universal embed player

--- lib/auto_html/filters/vimeo.rb
+++ lib/auto_html/filters/vimeo.rb
@@ -1,13 +1,15 @@
-AutoHtml.add_filter(:vimeo).with(:width => 440, :height => 248, :show_title => false, :fullscreen => true, :show_byline => false, :show_portrait => false) do |text, options|
+AutoHtml.add_filter(:vimeo).with(:width => 440, :height => 248, :show_title => false, :show_byline => false, :show_portrait => false) do |text, options|
   text.gsub(/http:\/\/(www.)?vimeo\.com\/([A-Za-z0-9._%-]*)((\?|#)\S+)?/) do
     vimeo_id = $2
     width  = options[:width]
     height = options[:height]
-    allowfullscreen = options[:fullscreen]
-    fullscreen      = options[:fullscreen]    ? 1 : 0 
-    show_title      = options[:show_title]    ? 1 : 0
-    show_byline     = options[:show_byline]   ? 1 : 0
-    show_portrait   = options[:show_portrait] ? 1 : 0
-    %{<object width="#{width}" height="#{height}"><param name="allowfullscreen" value="#{allowfullscreen}" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=#{vimeo_id}&amp;server=vimeo.com&amp;show_title=#{show_title}&amp;show_byline=#{show_byline}&amp;show_portrait=#{show_portrait}&amp;color=00adef&amp;fullscreen=#{fullscreen}" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=#{vimeo_id}&amp;server=vimeo.com&amp;show_title=#{show_title}&amp;show_byline=#{show_byline}&amp;show_portrait=#{show_portrait}&amp;color=00adef&amp;fullscreen=#{fullscreen}" type="application/x-shockwave-flash" allowfullscreen="#{allowfullscreen}" allowscriptaccess="always" width="#{options[:width]}" height="#{options[:height]}"></embed></object>}    
+    show_title      = "title=0"    unless options[:show_title]
+    show_byline     = "byline=0"   unless options[:show_byline]  
+    show_portrait   = "portrait=0" unless options[:show_portrait]
+    frameborder     = options[:frameborder] || 0
+    query_string_variables = [show_title, show_byline, show_portrait].compact.join("&")
+    query_string    = "?" + query_string_variables unless query_string_variables.empty?
+
+    %{<iframe src="http://player.vimeo.com/video/#{vimeo_id}#{query_string}" width="#{width}" height="#{height}" frameborder="#{frameborder}"></iframe>}
   end
 end
