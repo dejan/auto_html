f49768cad67d3adfec6cad6be7563f1a57b71983
Ryan Heneise <ryan@mysmallidea.com>
2010-11-29 20:59:44 -0600

    Added another test for markdown filter.

--- test/unit/filters/markdown_test.rb
+++ test/unit/filters/markdown_test.rb
@@ -11,5 +11,10 @@ class MarkdownTest < Test::Unit::TestCase
     result = auto_html('Hey check out my code => http://github.com/dejan') { markdown }
     assert_equal '<p>Hey check out my code => http://github.com/dejan</p>', result
   end
+  
+  def test_transform_list
+    result = auto_html("# A heading\n\n* A list\n* A list 2"){ markdown }
+    assert_equal "<h1>A heading</h1>\n\n<ul>\n<li>A list</li>\n<li>A list 2</li>\n</ul>", result
+  end
 
 end
