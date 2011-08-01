746a8291c1cb23c768674a7716d91041f0e4c082
Henning Thies <thies.henning@gmail.com>
2011-08-02 00:13:57 +0200

    replace if else with one regular expression

--- lib/auto_html/filters/youtube.rb
+++ lib/auto_html/filters/youtube.rb
@@ -1,11 +1,7 @@
 AutoHtml.add_filter(:youtube).with(:width => 390, :height => 250, :frameborder => 0) do |text, options|
-  if text =~ /youtube\.com/
-    regex = /http:\/\/(www.)?youtube\.com\/watch\?v=([A-Za-z0-9._%-]*)(\&\S+)?/
-  else
-    regex = /http:\/\/(www.)?youtu\.be\/([A-Za-z0-9._%-]*)(\&\S+)?/
-  end   
+  regex = /http:\/\/(www.)?youtube\.com\/watch\?v=([A-Za-z0-9._%-]*)(\&\S+)?|http:\/\/(www.)?youtu\.be\/([A-Za-z0-9._%-]*)?/
   text.gsub(regex) do
-    youtube_id = $2
+    youtube_id = $2 || $5
     width = options[:width]
     height = options[:height]
     frameborder = options[:frameborder]
