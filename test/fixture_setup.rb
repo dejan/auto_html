dcdb7134f193b02fe5b0e8ef95665567a04932d8
Dejan Simic <desimic@gmail.com>
2012-05-06 00:26:39 +0200

    connect to db before loading models

--- test/fixture_setup.rb
+++ test/fixture_setup.rb
@@ -1,8 +1,10 @@
 module FixtureSetup
+  fixtures_dir = File.dirname(__FILE__) + '/fixtures'
+  connections = YAML.load_file("#{fixtures_dir}/database.yml")
+  ActiveRecord::Base.establish_connection(connections['sqlite3'])
+  
   def setup
     fixtures_dir = File.dirname(__FILE__) + '/fixtures'
-    connections = YAML.load_file("#{fixtures_dir}/database.yml")
-    ActiveRecord::Base.establish_connection(connections['sqlite3'])
     ActiveRecord::Migration.verbose = false
     load "#{fixtures_dir}/schema.rb"
   end
