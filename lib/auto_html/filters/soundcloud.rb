96ab57e934c08af746b43da2dc6ee82f5bf07dc8
Samuel Goebert <hello@bigcurl.de>
2012-05-14 22:27:56 +0200

    parse links without protocol e.g. www.soundcloud.com/nice also it is possible to set the available options now

--- lib/auto_html/filters/soundcloud.rb
+++ lib/auto_html/filters/soundcloud.rb
@@ -1,10 +1,16 @@
 require 'uri'
 require 'net/http'
 
-AutoHtml.add_filter(:soundcloud).with(:maxwidth => '100%', :maxheight => '166', :auto_play => false, :show_comments => true) do |text, options|
-  text.gsub(/^(https?):\/\/(www.)?soundcloud\.com\/.*/) do |match|
+AutoHtml.add_filter(:soundcloud).with({}) do |text, options|
+  # set these options
+  # :maxwidth => '', :maxheight => '', :auto_play => false, :show_comments => false
+  text.gsub(/(https?:\/\/)?(www.)?soundcloud\.com\/.*/) do |match|
+    new_uri = match.to_s
+    new_uri = (new_uri =~ /^https?\:\/\/.*/) ? URI(new_uri) : URI("http://#{new_uri}")
+    new_uri.normalize!
+
     uri = URI("http://soundcloud.com/oembed")
-    params = {:format => 'json', :url => match}
+    params = {:format => 'json', :url => new_uri}
     params = params.merge options
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get(uri)
--- test/unit/filters/soundcloud_test.rb
+++ test/unit/filters/soundcloud_test.rb
@@ -12,8 +12,18 @@ class SoundcloudTest < Test::Unit::TestCase
     assert_equal '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F293&show_artwork=true"></iframe>', result
   end
 
+  def test_transform_url_without_protocol
+    result = auto_html('soundcloud.com/forss/flickermood') { soundcloud }
+    assert_equal '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F293&show_artwork=true"></iframe>', result
+  end
+
   def test_transform_url_with_ssl
     result = auto_html('https://soundcloud.com/forss/flickermood') { soundcloud }
     assert_equal '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F293&show_artwork=true"></iframe>', result
+  end
+
+  def test_transform_url_with_options
+    result = auto_html('http://www.soundcloud.com/forss/flickermood') { soundcloud(:maxwidth => '100', :maxheight => '100', :auto_play => false, :show_comments => false) }
+    assert_equal '<iframe width="100" height="100" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F293&show_artwork=true&maxwidth=100&maxheight=100&auto_play=false&show_comments=false"></iframe>', result
   end
 end
