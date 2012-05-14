49570b929c354caf99dd982cffbf2fcef9ccf3dc
Samuel Goebert <hello@bigcurl.de>
2012-05-14 16:39:03 +0200

    Added options for customizing the widget submitted wrong url to soundcloud catch empty responses from soundcloud eg. service is down

--- lib/auto_html/filters/soundcloud.rb
+++ lib/auto_html/filters/soundcloud.rb
@@ -1,11 +1,18 @@
 require 'uri'
 require 'net/http'
 
-AutoHtml.add_filter(:soundcloud).with({}) do |text, options|
-  text.gsub(/(https?):\/\/(www.)?soundcloud\.com\/.*/) do
+AutoHtml.add_filter(:soundcloud).with(:maxwidth => '100%', :maxheight => '166', :auto_play => false, :show_comments => true) do |text, options|
+  text.gsub(/^(https?):\/\/(www.)?soundcloud\.com\/.*/) do |match|
     uri = URI("http://soundcloud.com/oembed")
-    uri.query = URI.encode_www_form({:format => 'json', :url => text})
-    response = JSON.parse(Net::HTTP.get(uri))
-    response["html"]
+    params = {:format => 'json', :url => match}
+    params = params.merge options
+    uri.query = URI.encode_www_form(params)
+    response = Net::HTTP.get(uri)
+
+    if !response.blank?
+      JSON.parse(response)["html"]
+    else
+      match
+    end
   end
 end
