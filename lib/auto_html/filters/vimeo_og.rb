153963fdb61b26dd3db7d878838c7ce9602ac7ce
Jorge F. Contreras <windblows3@yahoo.com>
2012-06-13 15:25:18 -0500

    addin og image xtraction for vimeo

---
+++ lib/auto_html/filters/vimeo_og.rb
@@ -0,0 +1,14 @@
+AutoHtml.add_filter(:vimeo).with(:width => 440, :height => 248, :show_title => false, :show_byline => false, :show_portrait => false) do |text, options|
+    text.gsub(/(https?):\/\/(www.)?vimeo\.com\/([A-Za-z0-9._%-]*)((\?|#)\S+)?/) do
+		p = HTTParty.get(text)
+		s = p.to_s
+		a = s.rindex('og:image')
+		b = a+19
+		img = []
+		while s[b] != '"' do
+			img << s[b]
+			b = b + 1
+		end
+		img.join
+    end
+end
