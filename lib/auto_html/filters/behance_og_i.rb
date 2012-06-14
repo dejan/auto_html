72f9b04522270ffe90b8c5b877741a9fb711964e
Jorge F. Contreras <windblows3@yahoo.com>
2012-06-14 01:32:14 -0500

    vimeo_og

---
+++ lib/auto_html/filters/behance_og_i.rb
@@ -0,0 +1,24 @@
+require 'redcarpet'
+
+class NoParagraphRenderer < ::Redcarpet::Render::XHTML
+  def paragraph(text)
+    text
+  end    
+end
+
+AutoHtml.add_filter(:soundcloud_og).with({:alt => ''}) do |text, options|
+	alt = options[:alt]
+	r = Redcarpet::Markdown.new(NoParagraphRenderer)
+	text.gsub(/(https?):\/\/(www.)?soundcloud\.com\//) do
+		p = HTTParty.get(text)
+		s = p.to_s
+		a = s.rindex('og:image')
+		b = a+19
+		img = []
+		while s[b] != '"' do
+			img << s[b]
+			b = b + 1
+		end
+    	r.render("![#{alt}](#{img.reverse.join})")
+    end
+end
