72e50298865e9d6bd40ad0998b7bed60f84968a7
Jorge F. Contreras <windblows3@yahoo.com>
2012-06-14 01:24:26 -0500

    vimeo_og

--- lib/auto_html/filters/soundcloud_og.rb
+++ lib/auto_html/filters/soundcloud_og.rb
@@ -7,12 +7,9 @@ class NoParagraphRenderer < ::Redcarpet::Render::XHTML
 end
 
 AutoHtml.add_filter(:soundcloud_og).with({:alt => ''}) do |text, options|
-	text.gsub(/(https?):\/\/(www.)?soundcloud\.com\//) do
 	alt = options[:alt]
 	r = Redcarpet::Markdown.new(NoParagraphRenderer)
-
-  	regex = /(https?):\/\/(www.)?(youtube\.com\/watch\?v=|youtu\.be\/)([A-Za-z0-9_-]*)(\&\S+)?(\S)*/
- 	text.gsub(regex) do
+	text.gsub(/(https?):\/\/(www.)?soundcloud\.com\//) do
 		p = HTTParty.get(text)
 		s = p.to_s
 		a = s.rindex('og:image')
