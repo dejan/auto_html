9e4078bddaf1c205581daadd734c574eddc287bf
Dejan Simic <desimic@gmail.com>
2012-07-29 01:35:49 +0200

    fix #42: rake task fails

--- lib/auto_html/rake_tasks.rb
+++ lib/auto_html/rake_tasks.rb
@@ -7,24 +7,21 @@ require 'auto_html/task'
 namespace :auto_html do
   desc "Rebuild auto_html columns"
   task :rebuild => :environment do
-    klass = AutoHtml::Task.obtain_class.constantize
+
+    klass  = AutoHtml::Task.obtain_class.constantize
+    suffix = AutoHtmlFor.auto_html_for_options[:htmlized_attribute_suffix]
+    column_names = klass.respond_to?(:column_names) ? klass.column_names : klass.fields.keys
+    observed_attributes = column_names.select { |c| c=~/#{suffix}$/ }.map { |c| c.gsub(/#{suffix}$/, '')}
+
     i = 0
-    auto_html_fields = []
     klass.find_each do |item|
-      unless auto_html_fields.any?
-        auto_html_methods = item.methods.select { |m| m=~/^auto_html_prepare_/ }
-        auto_html_methods.each do |method|
-          auto_html_fields << method
-        end
+      observed_attributes.each do |field|
+        item.send("#{field}=", item.send(field))
       end
-      
-      auto_html_fields.each do |field|
-        item.send(field)
-      end
-      
       item.save
       i += 1
     end
+
     puts "Done! Processed #{i} items."
   end
 end
