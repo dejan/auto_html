2214f63a19aa31c4c5833f8a8f74abbed909ff1d
Alex <alex.guerrilla@gmail.com>
2012-08-23 03:08:17 -0400

    Added a unit test for worldstar

--- lib/auto_html/filters/worldstar.rb
+++ lib/auto_html/filters/worldstar.rb
@@ -1,5 +1,5 @@
 AutoHtml.add_filter(:worldstar).with(:width => 448, :height => 374) do |text,options|
-    text.gsub(/http:\/\/www\.worldstarhiphop\.com\/videos\/video\.php\?v\=(wshh[A-Za-z0-9]+)/) do
+    text.gsub(/http:\/\/www\.worldstarhiphop\.com\/videos\/video\.php\?v\=(wshh[A-Za-z0-9]+)/) do 
       video_id = $1
       width  = options[:width]
       height = options[:height]
