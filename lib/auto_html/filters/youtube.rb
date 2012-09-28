f4124cf22027753082b340aeba3d85a085327da4
Alex Denisov <1101.debian@gmail.com>
2012-09-28 09:46:25 +0300

    Youtube: autoplay option added

--- lib/auto_html/filters/youtube.rb
+++ lib/auto_html/filters/youtube.rb
@@ -1,4 +1,4 @@
-AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil) do |text, options|
+AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil, :autoplay => false) do |text, options|
   regex = /https?:\/\/(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=player_embedded&v=)([A-Za-z0-9_-]*)(\&\S+)?(\S)*/
   text.gsub(regex) do
     youtube_id = $3
@@ -6,8 +6,12 @@ AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder =
     height = options[:height]
     frameborder = options[:frameborder]
 		wmode = options[:wmode]
-		src = "//www.youtube.com/embed/#{youtube_id}"
-		src += "?wmode=#{wmode}" if wmode
+    autoplay = options[:autoplay]
+		src = "//www.youtube.com/embed/#{youtube_id}?"
+    params = []
+		params << "wmode=#{wmode}" if wmode
+    params << "autoplay=#{autoplay ? 1 : 0}" if autoplay
+    src += params.join '&' unless params.empty?
     %{<iframe width="#{width}" height="#{height}" src="#{src}" frameborder="#{frameborder}" allowfullscreen></iframe>}
   end
 end
