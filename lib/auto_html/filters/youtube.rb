99d5f303319e3ee2d35e22e41cd540e11911fc24
Johannes <jacubeit@gmail.com>
2012-12-19 00:06:11 +0100

    fixed typo

--- lib/auto_html/filters/youtube.rb
+++ lib/auto_html/filters/youtube.rb
@@ -1,4 +1,4 @@
-AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil, :hd => 1, :rel => 0, :rel => 0, :fs => 1, :controls => 1, :showinfo => 1, :modestbranding => 0, :title => true) do |text, options|
+AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil, :hd => 1, :rel => 0, :rel => 0, :fs => 1, :controls => 1, :showinfo => 1, :modestbranding => 0, :title => "") do |text, options|
   regex = /https?:\/\/(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=player_embedded&v=)([A-Za-z0-9_-]*)(\&\S+)?(\S)*/
   text.gsub(regex) do
     youtube_id = $3
