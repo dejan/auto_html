d15b4ca1b79e3eac052aabebafb2ff484f80228c
Johannes <jacubeit@gmail.com>
2012-12-18 23:44:21 +0100

    ads modestbranding as option

--- lib/auto_html/filters/youtube.rb
+++ lib/auto_html/filters/youtube.rb
@@ -1,4 +1,4 @@
-AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil, :hd => 1, :rel => 0, :rel => 0, :fs => 1, :controls => 1, :showinfo => 1) do |text, options|
+AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil, :hd => 1, :rel => 0, :rel => 0, :fs => 1, :controls => 1, :showinfo => 1, :modestbranding => 0) do |text, options|
   regex = /https?:\/\/(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=player_embedded&v=)([A-Za-z0-9_-]*)(\&\S+)?(\S)*/
   text.gsub(regex) do
     youtube_id = $3
@@ -11,7 +11,8 @@ AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder =
     fs = options[:fs]
     controls = options[:controls]
     showinfo = options[:showinfo]
-    src = "//www.youtube.com/embed/#{youtube_id}?rel=#{rel}&fs=#{fs}&HD=#{hd}&showinfo=#{showinfo}&controls=#{controls}"
+    modestbranding = options[:modestbranding]
+    src = "//www.youtube.com/embed/#{youtube_id}?rel=#{rel}&fs=#{fs}&HD=#{hd}&showinfo=#{showinfo}&controls=#{controls}&modestbranding=#{modestbranding}"
     src += "?wmode=#{wmode}" if wmode
     %{<iframe title="YouTube video player" width="#{width}" height="#{height}" src="#{src}" frameborder="#{frameborder}" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>}
   end
