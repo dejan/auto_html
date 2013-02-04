4fb024aa15579d4d3fd7452b3c49ce0fe3a75a6e
Jay Strybis <jay.strybis@gmail.com>
2013-02-04 08:38:26 -0600

    allow a :proxy option on image filter

--- lib/auto_html/filters/image.rb
+++ lib/auto_html/filters/image.rb
@@ -3,13 +3,14 @@ require 'redcarpet'
 class NoParagraphRenderer < ::Redcarpet::Render::XHTML
   def paragraph(text)
     text
-  end    
+  end
 end
 
 AutoHtml.add_filter(:image).with({:alt => ''}) do |text, options|
   r = Redcarpet::Markdown.new(NoParagraphRenderer)
   alt = options[:alt]
+  options[:proxy] ||= ""
   text.gsub(/https?:\/\/.+?\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
-    r.render("![#{alt}](#{match})")
+    r.render("![#{alt}](#{options[:proxy]}#{match})")
   end
 end
