ca5a971952d2d178c99671c8c54e7a4782d6a4d7
tanraya <demerest@gmail.com>
2013-08-27 10:29:46 +0600

    Deferred loading of filters dependencies when possible

--- lib/auto_html/filters/link.rb
+++ lib/auto_html/filters/link.rb
@@ -1,7 +1,7 @@
-require 'rinku'
-require 'rexml/document'
-
 AutoHtml.add_filter(:link).with({}) do |text, options|
+  require 'rinku'
+  require 'rexml/document'
+
   attributes = Array(options).reject { |k,v| v.nil? }.map { |k, v| %{#{k}="#{REXML::Text::normalize(v)}"} }.join(' ')
   Rinku.auto_link(text, :all, attributes)
 end
