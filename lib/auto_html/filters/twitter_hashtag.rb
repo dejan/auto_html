05748b2a5e4a66250c38669b0a95ada6dd7b0053
Nícolas Iensen <nicolas@iensen.me>
2013-09-25 14:16:47 -0300

    Add support to Facebook on the hashtag filter

--- lib/auto_html/filters/twitter_hashtag.rb
+++
@@ -1,3 +0,0 @@
-AutoHtml.add_filter(:twitter_hashtag) do |text, options|
-  text.gsub(/#([^ ]+)/, '<a href="http://twitter.com/search?q=%23\1&f=realtime" class="hashtag" target="_blank">#\1</a>')
-end
