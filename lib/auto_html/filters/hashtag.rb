8ba403a79ec6712fe6c37dde43cde8b7abdef24c
Nícolas Iensen <nicolas@iensen.me>
2013-09-25 14:23:44 -0300

    Set default source to the hashtag filter

--- lib/auto_html/filters/hashtag.rb
+++ lib/auto_html/filters/hashtag.rb
@@ -1,4 +1,4 @@
-AutoHtml.add_filter(:hashtag) do |text, options|
+AutoHtml.add_filter(:hashtag).with(:source => :twitter) do |text, options|
   if options[:source] == :twitter
     text.gsub(/#([^ ]+)/, '<a href="http://twitter.com/search?q=%23\1&f=realtime" class="hashtag" target="_blank">#\1</a>')
   elsif options[:source] == :facebook
