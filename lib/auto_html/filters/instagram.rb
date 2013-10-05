1c4a2ef432208e4fccabf21d1edf62debeccce20
Brian Runnells <brian.runnells@gmail.com>
2013-10-05 09:47:35 -0600

    Fixing formatting of URL

--- lib/auto_html/filters/instagram.rb
+++ lib/auto_html/filters/instagram.rb
@@ -5,6 +5,6 @@ AutoHtml.add_filter(:instagram) do |text|
   regex = %r{https?:\/\/(www.)?instagr(am\.com|\.am)/p/.+}
   
   text.gsub(regex) do
-    image = URI("#{text}/embed/")
+    image = URI("#{text}embed/")
   end
 end
