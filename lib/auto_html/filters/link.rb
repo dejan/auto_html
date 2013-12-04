87156b6541e63d55628d0665c75f2e2bf236a02f
Samuel Goebert <hello@bigcurl.de>
2013-12-05 00:17:40 +0100

    save uri strings

--- lib/auto_html/filters/link.rb
+++ lib/auto_html/filters/link.rb
@@ -7,7 +7,7 @@ AutoHtml.add_filter(:link).with({}) do |text, options|
   attributes = Array(options).reject { |k,v| v.nil? }.map { |k, v| %{#{k}="#{REXML::Text::normalize(v)}"} }.join(' ')
   Rinku.auto_link(text, :all, attributes) do |url|
     if option_short_link_name
-      uri = URI(url)
+      uri = URI.parse(URI.encode(url.strip))
       uri.query = nil
       uri.to_s
     else
