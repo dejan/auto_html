a1f064e3b88264d72afccb8198e1eb962b89f614
Phlegx Systems OG <register@phlegx.com>
2013-12-18 23:50:21 +0100

    Add class attribute to metacafe filter.

--- lib/auto_html/filters/metacafe.rb
+++ lib/auto_html/filters/metacafe.rb
@@ -8,6 +8,6 @@ AutoHtml.add_filter(:metacafe).with(:width => 440, :height => 272, :show_stats =
     autoplay        = options[:autoplay] ? "autoPlay=yes" : "autoPlay=no"
     flash_vars = [show_stats, autoplay].join("|")
 
-    %{<divclass="auto-html-embed metacafe-embed" style="background:#000000;width:#{width}px;height:#{height}px"><embed flashVars="playerVars=#{flash_vars}" src="http://www.metacafe.com/fplayer/#{metacafe_id}/#{metacafe_slug}.swf" width="#{width}" height="#{height}" wmode="transparent" allowFullScreen="true" allowScriptAccess="always" name="Metacafe_#{metacafe_id}" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash"></embed></div>}
+    %{<div class="auto-html-embed metacafe-embed" style="background:#000000;width:#{width}px;height:#{height}px"><embed flashVars="playerVars=#{flash_vars}" src="http://www.metacafe.com/fplayer/#{metacafe_id}/#{metacafe_slug}.swf" width="#{width}" height="#{height}" wmode="transparent" allowFullScreen="true" allowScriptAccess="always" name="Metacafe_#{metacafe_id}" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash"></embed></div>}
   end
 end
