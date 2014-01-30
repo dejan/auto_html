ed578d6c91f8c8505a49787edf0a40e58625563d
Brian Runnells <brian.runnells@gmail.com>
2014-01-29 21:33:15 -0700

    Ensure Instagram URL has trailing slash

--- lib/auto_html/filters/instagram.rb
+++ lib/auto_html/filters/instagram.rb
@@ -2,8 +2,9 @@ require 'uri'
 require 'net/http'
 
 AutoHtml.add_filter(:instagram) do |text|
+  text << '/' unless text.end_with?('/')
   regex = %r{https?:\/\/(www.)?instagr(am\.com|\.am)/p/.+}
   text.gsub(regex) do
-    image = URI("#{text}/embed/")
+    image = URI("#{text}embed/")
   end
 end
