f59d676efe1897717dc247190b6d1936a2085128
Brian Runnells <brian.runnells@gmail.com>
2014-01-29 21:41:03 -0700

    Switch to straight iframe method

--- lib/auto_html/filters/instagram.rb
+++ lib/auto_html/filters/instagram.rb
@@ -1,10 +1,12 @@
 require 'uri'
 require 'net/http'
 
-AutoHtml.add_filter(:instagram) do |text|
+AutoHtml.add_filter(:instagram).with(:width => 710, :height => 612) do |text|
   text << '/' unless text.end_with?('/')
+  width = options[:width]
+  height = options[:height]
   regex = %r{https?:\/\/(www.)?instagr(am\.com|\.am)/p/.+}
   text.gsub(regex) do
-    image = URI("#{text}embed/")
+    %{<iframe src="#{text}embed" height="#{width}" width="#{height}" frameborder="0" scrolling="no"></iframe>}
   end
 end
