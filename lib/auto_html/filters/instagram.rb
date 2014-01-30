4c0c2903882cea0541b421a12681fa33370a1590
Brian Runnells <brian.runnells@gmail.com>
2014-01-29 21:13:31 -0700

    Add Instagram filter & tests

---
+++ lib/auto_html/filters/instagram.rb
@@ -0,0 +1,9 @@
+require 'uri'
+require 'net/http'
+
+AutoHtml.add_filter(:instagram) do |text|
+  regex = %r{https?:\/\/(www.)?instagr(am\.com|\.am)/p/.+}
+  text.gsub(regex) do
+    image = URI("#{text}/embed/")
+  end
+end
