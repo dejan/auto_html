0bed67bde02b2274c89ac572acbc40ae1dcc00f3
Brian Runnells <brian.runnells@gmail.com>
2014-01-30 21:24:14 -0700

    Fix test so it passes

--- test/unit/filters/instagram_test.rb
+++ test/unit/filters/instagram_test.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../unit_test_helper', __FILE__)
 
 class InstagramTest < Test::Unit::TestCase
   def test_instagram_embed
-    result = auto_html('http://instagr.am/p/WsQTLAGvx7/') { instagram }
+    result = auto_html('http://instagram.com/p/WsQTLAGvx7/') { instagram }
     assert_equal '<iframe src="//instagram.com/p/WsQTLAGvx7/embed/" height="714" width="616" frameborder="0" scrolling="no"></iframe>', result
   end
 end
