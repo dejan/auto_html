7d9b61e932d0a891aa73cf93b8986ac86eb68675
koshikawa <koshikawa@ppworks.jp>
2014-03-04 00:36:17 +0900

    Add test; gist

--- test/unit/filters/gist_test.rb
+++ test/unit/filters/gist_test.rb
@@ -4,12 +4,12 @@ class GistTest < Test::Unit::TestCase
 
   def test_transform
     result = auto_html('https://gist.github.com/1710276') { gist }
-    assert_equal '<script src="https://gist.github.com/1710276.js"></script>', result
+    assert_equal '<script type="text/javascript" src="https://gist.github.com/1710276.js"></script>', result
   end
 
   def test_transform_with_username
     result = auto_html('https://gist.github.com/toctan/6547840') { gist }
-    assert_equal '<script src="https://gist.github.com/6547840.js"></script>', result
+    assert_equal '<script type="text/javascript" src="https://gist.github.com/6547840.js"></script>', result
   end
 
 end
