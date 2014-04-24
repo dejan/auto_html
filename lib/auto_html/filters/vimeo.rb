f0fc70a73f01239f7ecc682b28169e70b829d174
Seth Banks <stb@subimage.com>
2014-04-23 18:38:25 -0700

    Wrap all video filters with standard DIVs for CSS control.
    Allows us to control display for responsive videos. Also updated tests
    to match.

--- lib/auto_html/filters/vimeo.rb
+++ lib/auto_html/filters/vimeo.rb
@@ -10,6 +10,6 @@ AutoHtml.add_filter(:vimeo).with(:width => 440, :height => 248, :show_title => f
     query_string_variables = [show_title, show_byline, show_portrait].compact.join("&")
     query_string    = "?" + query_string_variables unless query_string_variables.empty?
 
-    %{<iframe src="//player.vimeo.com/video/#{vimeo_id}#{query_string}" width="#{width}" height="#{height}" frameborder="#{frameborder}"></iframe>}
+    %{<div class="video-container vimeo"><iframe src="//player.vimeo.com/video/#{vimeo_id}#{query_string}" width="#{width}" height="#{height}" frameborder="#{frameborder}"></iframe></div>}
   end
 end
