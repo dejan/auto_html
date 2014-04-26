e056442eac1a2ca8e1b6d6fe8d8c754a232eb681
Seth Banks <stb@subimage.com>
2014-04-25 20:52:57 -0700

    Fixed bug where instagram filter was appending slash
    Added more tests to prevent regressions

--- lib/auto_html/filters/instagram.rb
+++ lib/auto_html/filters/instagram.rb
@@ -2,9 +2,9 @@ require 'uri'
 require 'net/http'
 
 AutoHtml.add_filter(:instagram) do |text|
-  text << '/' unless text.end_with?('/')
   regex = %r{https?:\/\/(www.)?instagr(am\.com|\.am)/p/.+}
-  text.gsub(regex) do
-    %{<iframe src="#{text}embed" height="714" width="616" frameborder="0" scrolling="no"></iframe>}
+  text.gsub(regex) do |match|
+    match << '/' unless match.end_with?('/')
+    %{<iframe src="#{match}embed" height="714" width="616" frameborder="0" scrolling="no"></iframe>}
   end
 end
