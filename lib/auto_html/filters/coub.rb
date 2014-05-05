c77e807aab4261cc33784f507dceb9d75b23583f
Anton Davydov <don.mist@gmail.com>
2014-05-05 19:16:31 +0400

    Fix regex for yutube, coub and vimeo links

--- lib/auto_html/filters/coub.rb
+++ lib/auto_html/filters/coub.rb
@@ -1,5 +1,5 @@
 AutoHtml.add_filter(:coub).with(:width => 640, :height => 480, :frameborder => 0, :allowfullscreen => true) do |text, options|
-  regex = /(http?:\/\/)?(www.)?(coub\.com\/view\/([A-Za-z0-9]*))/
+  regex = /(http?:\/\/)?(www.)?(coub\.com\/view\/([A-Za-z0-9]*))(\/.+)?/
   text.gsub(regex) do
     coub_id = $4
     width = options[:width]
