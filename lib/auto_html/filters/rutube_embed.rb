542a013a1d79d7982469b81d202d3c9ea877ccb4
Anton Davydov <don.mist@gmail.com>
2014-07-03 13:18:19 +0400

    Filters for embed youtube, vimeo and rutube video services

---
+++ lib/auto_html/filters/rutube_embed.rb
@@ -0,0 +1,11 @@
+AutoHtml.add_filter(:rutube_embed).with(width: 640, height: 480, frameborder: 0) do |text, options|
+  regex = /(https?:\/\/)?(www.)?rutube\.ru\/(play|video)\/embed\/([A-Za-z0-9]*)(\/.*)?/
+  text.gsub(regex) do
+    rutube_id = $4
+    width = options[:width]
+    height = options[:height]
+    frameborder = options[:frameborder]
+    src = "//rutube.ru/video/embed/#{rutube_id}"
+    %{<iframe src="#{src}" width="#{width}" height="#{height}" frameborder="#{frameborder}" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>}
+  end
+end
---
+++ lib/auto_html/filters/vimeo_embed.rb
@@ -0,0 +1,15 @@
+AutoHtml.add_filter(:vimeo_embed).with(width: 440, height: 248, show_title: false, show_byline: false, show_portrait: false, frameborder: 0) do |text, options|
+  text.gsub(/(https?:\/\/)?player.vimeo\.com\/video\/([A-Za-z0-9._%-]*)((\?|#)\S+)?(\/.+)?/) do
+    vimeo_id = $2
+    width  = options[:width]
+    height = options[:height]
+    show_title      = "title=0"    unless options[:show_title]
+    show_byline     = "byline=0"   unless options[:show_byline]
+    show_portrait   = "portrait=0" unless options[:show_portrait]
+    frameborder     = options[:frameborder] || 0
+    query_string_variables = [show_title, show_byline, show_portrait].compact.join("&")
+    query_string    = "?" + query_string_variables unless query_string_variables.empty?
+
+    %{<iframe src="//player.vimeo.com/video/#{vimeo_id}#{query_string}" width="#{width}" height="#{height}" frameborder="#{frameborder}" allowfullscreen></iframe>}
+  end
+end
