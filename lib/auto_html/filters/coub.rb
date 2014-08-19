503ad4c0333b53e6f8b36fa5b1b5b652623ed1dc
Anton Davydov <don.mist@gmail.com>
2014-08-19 12:17:48 +0400

    Replase coub embed match to one file

--- lib/auto_html/filters/coub.rb
+++ lib/auto_html/filters/coub.rb
@@ -1,7 +1,7 @@
 AutoHtml.add_filter(:coub).with(width: 640, height: 480, frameborder: 0, allowfullscreen: true) do |text, options|
-  regex = /(http?:\/\/)?(www.)?(coub\.com\/view\/([A-Za-z0-9]*))(\/.+)?/
+  regex = /(http?:\/\/)?(www.)?(coub\.com\/(view|embed)\/([A-Za-z0-9]*))(\/.+)?/
   text.gsub(regex) do
-    coub_id = $4
+    coub_id = $5
     width = options[:width]
     height = options[:height]
     frameborder = options[:frameborder]
