f28344cad3991ea4b29472fe52faa6520cfcad81
hugoabonizio <hugo_abonizio@hotmail.com>
2015-06-22 16:16:09 +0000

    Comments

--- lib/auto_html/filters/sanitize.rb
+++ lib/auto_html/filters/sanitize.rb
@@ -1,9 +1,11 @@
 AutoHtml.add_filter(:sanitize).with({}) do |text, options|
   require 'action_controller'
   require 'cgi'
-  if Rails::VERSION::MAJOR == 4 and Rails::VERSION::MINOR == 2
+  if Rails::VERSION::MAJOR == 4 and Rails::VERSION::MINOR >= 2
+    # Rails >=4.2
     Rails::Html::WhiteListSanitizer.new.sanitize(text, options)
   else
+    # Rails <4.2
     HTML::WhiteListSanitizer.new.sanitize(text, options)
   end
 end
--- test/unit/filters/sanitize_test.rb
+++ test/unit/filters/sanitize_test.rb
@@ -24,9 +24,11 @@ class SanitizeTest < Minitest::Test
 
   def test_trasform5
     result = auto_html("<a rel='nofollow'>test</div>") { sanitize :tags => %w(a), :attributes => %w(href) }
-    if Rails::VERSION::MAJOR == 4 and Rails::VERSION::MINOR == 2
+    if Rails::VERSION::MAJOR == 4 and Rails::VERSION::MINOR >= 2
+      # Rails >=4.2
       assert_equal "<a>test</a>", result
     else
+      # Rails <4.2
       assert_equal "<a>test", result
     end
     #
