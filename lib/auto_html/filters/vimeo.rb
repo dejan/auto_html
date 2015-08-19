00e576b9db90be5f370246e4d1f95ba9b44c8387
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2015-08-19 16:25:50 +0200

    add div wrapper for iframe
    make it consistent with how youtube filter works

--- lib/auto_html/filters/vimeo.rb
+++ lib/auto_html/filters/vimeo.rb
@@ -11,6 +11,6 @@ AutoHtml.add_filter(:vimeo).with(:width => 440, :height => 248, :show_title => f
     query_string_variables = [show_title, show_byline, show_portrait].compact.join("&")
     query_string    = "?" + query_string_variables unless query_string_variables.empty?
 
-    %{<iframe src="//player.vimeo.com/video/#{vimeo_id}#{query_string}" width="#{width}" height="#{height}" frameborder="#{frameborder}"#{allow_fullscreen}></iframe>}
+    %{<div class="video vimeo"><iframe src="//player.vimeo.com/video/#{vimeo_id}#{query_string}" width="#{width}" height="#{height}" frameborder="#{frameborder}"#{allow_fullscreen}></iframe></div>}
   end
 end
